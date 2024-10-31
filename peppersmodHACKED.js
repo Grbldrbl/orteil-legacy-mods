8aff86d5664085582fca41a69f572a1412ef5dbb
Grbldrbl <grbldrbl@gmail.com>
2024-10-31 15:50:09 -0600

    Create peppersmodHACKED.js

---
+++ peppersmodHACKED.js
@@ -0,0 +1,62 @@
+G.AddData({
+name:'peppers HACKED',
+author:'original, Orteil HACKED, grbldrbl',
+desc:'a HACKED mod that adds hot peppers and hot sauce but they are HACKED.',
+engineVersion:1,
+manifest:'modManifest.js',
+requires:['Default dataset*'],
+sheets:{'spicySheet':'https://brunosupremo.github.io/market_mod/market_images.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
+	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
+	
+	//First we create a couple new resources :
+	new G.Res({
+		name:'hot pepper YALL',
+		desc:'[hot pepper]s are loaded with capsaicin and, depending on who you ask, may produce a pleasant burn when eaten YALL.',
+		icon:[0,0,'spicySheet'],
+		turnToByContext:{'eat':{'health':9999999999999999,'happiness':12345678911121314151617181920},'decay':{'spoiled food':0}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+		partOf:'food',
+		category:'food',
+	});
+	new G.Res({
+		name:'hot sauce YALL',
+		desc:'Made from [herb]s and the [hot pepper,Spiciest peppers], this [hot sauce] stays fresh for a while and will leave anyone panting and asking for more.',
+		icon:[1,0,'spicySheet'],
+		turnToByContext:{'eat':{'health':999999,'happiness':123456789},'decay':{'hot sauce':0.95,'spoiled food':0.05}},//that last part makes hot sauce effectively have a 95% chance of simply not rotting (in effect, it decays into itself)
+		partOf:'food',
+		category:'food',
+	});
+	
+	//Then we augment the base data to incorporate our new resources :
+		//adding hot pepper as something that can be gathered from grass
+	G.getDict('grass').res['gather']['hot pepper']=3;
+		//adding a new mode to artisans so they can make hot sauce from hot peppers
+	G.getDict('artisan').modes['hot sauce']={name:'Make hot sauce',desc:'Turn 3 [hot pepper]s and 3 [herb]s into 1 [hot sauce].',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
+		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
+	G.getDict('artisan').effects.push({type:'convert',from:{'hot pepper':3,'herb':3},into:{'hot sauce':1},every:3,mode:'hot sauce'});
+	
+	//Then we add a new technology which is required by the artisans to gain access to the "hot sauce" mode :
+	new G.Tech({
+		name:'hot sauce preparing',
+		desc:'@[artisan]s can now produce [hot sauce] from [hot pepper]s and [herb]s//This special recipe allows a skilled craftsman to fully express the complex aromas present in hot peppers.',
+		icon:[0,1,'spicySheet'],
+		cost:{'insight':0},
+		req:{},
+	});
+	
+	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
+	new G.Trait({
+		name:'hot sauce madness',
+		desc:'@your people appreciate [hot sauce] twice as much and will be twice as happy from consuming it.',
+		icon:[1,1,'spicySheet'],
+		chance:20,
+		req:{'hot sauce preparing':true},
+		effects:[
+			{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=123456789012345678901234567890;}},//this is a custom function executed when we gain the trait
+		],
+	});
+	
+	//There are many other ways of adding and changing content; refer to /data.js, the default dataset, if you want to see how everything is done in the base game. Good luck!
+}
+});
