af4a180a2f402ce78ad9b74133f6634c976a0912
Grbldrbl <grbldrbl@gmail.com>
2024-11-02 16:32:16 -0600

    Update gmailmodnothacked.js

--- gmailmodnothacked.js
+++ gmailmodnothacked.js
@@ -5,22 +5,19 @@ desc:'A gmail mod',
 engineVersion:1,
 manifest:0,
 requires:['Default dataset*'],
-sheets:{'gmailSheet' 'grbldrbl.github.io/orteil-legacy-mods/gmailSheet.png'},//custom stylesheet
+sheets:{'gmailSheet' 'grbldrbl.github.io/orteil-legacy-mods/gmailSheet.png'},
 func:function()
 {
 	new G.Res({
 		name:'mail',
-		desc:'[mail]s are loaded with text for stuff.',
+		desc:'mails are loaded with text for stuff.',
 		icon:[0,0],
 		turnToByContext:{'eat':{'health':0,'happiness':55},
 		partOf:'food',
 		category:'Miscellaneous',
 		req{'computer tech':true}
 	});
-	
-	//Then we augment the base data to incorporate our new resources :
-		//adding hot pepper as something that can be gathered from grass
-	G.getDict('grass').res['gather']['mail']=5;
+	G.getDict('grass').res['Gather']['mail']=5;
 	
 	new G.Tech({
 		name:'computer tech',
@@ -29,19 +26,15 @@ func:function()
 		cost:{'insight':10},
 		req:{},
 	});
-	
-	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Computer tech" tech) is fulfilled.
 	new G.Trait({
 		name:'gmail madness',
 		desc:'@your people appreciate [mail] twice as much and will be twice as happy from consuming it.',
-		icon:[0,1,'spicySheet'],
-		chance:20,
+		icon:[0,1,'gmailSheet'],
+		chance:1,
 		req:{'computer tech':true},
 		effects:[
-			{type:'function',func:function(){G.getDict('mail').turnToByContext['eat']['happiness']=120;}},//this is a custom function executed when we gain the trait
+			{type:'function',func:function(){G.getDict('mail').turnToByContext['eat']['happiness']=12;}}
 		],
 	});
-	
-	//There are many other ways of adding and changing content; refer to /data.js, the default dataset, if you want to see how everything is done in the base game. Good luck!
 }
 });
