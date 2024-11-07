66af78939c9970a258a31398ea89329a4b4d07ee
Grbldrbl <grbldrbl@gmail.com>
2024-11-07 16:31:02 -0700

    Update computersmod.js

--- computersmod.js
+++ computersmod.js
@@ -4,14 +4,39 @@ G.AddData({
   author: "Grbldrbl",
   desc: "a mod that adds computer tech.",
   engineVersion: 1,
-  sheets: {"customSheet":"https://grbldrbl.github.io/orteil-legacy-mods/gmailSheet.png"},
+  sheets: {"customSheet":'https://grbldrbl.github.io/orteil-legacy-mods/gmailSheet.png'},
 	func:function() {
 		G.unitCategories.unshift({
 			id:'Compyoutah-tek',
 			name:'computers'
 });
     new G.Tech({name:'computertech',displayName:'computer tech',icon:['customSheet', 0,0],cost:{insight:1},req:{language:true},desc:'adds [computer]s.',category:'computers',startWith:'0',chance:'1',tutorialMesg:'jnj'})
- 
+
+	new G.Res({
+		name:'mail',
+		desc:'[mail] is G.',
+		icon:[0,1],
+		turnToByContext:{'eat':{'health':0.01,'happiness':0.03}},
+		partOf:'food',
+		category:'food',
+	});
+		
+		let compute_modes = {
+			'off':G.MODE_OFF,
+			'mail':{
+				name:'mail',
+				icon: [0,1],
+				desc:'makes nothing right now.'
+			};
+	let compute_effects = [
+		{
+			mode:'mail',
+			type:'convert',
+			from:{'sand':1},
+			into:{'mail':1},
+			every:3
+		};
+		
 	  new G.Unit({
 			name:'computer',
 			displayName:'Computer',
@@ -23,6 +48,8 @@ G.AddData({
 				'land':1,
 			},
 			gizmos:true,
+		  modes: 'compute_modes',
+		  effects: 'compute_effects',
 		  category:'Compyoutah-tek',
 });
 
